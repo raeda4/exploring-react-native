abbd8d6c5ad369034625a36749074313b89423ca
Dambach <rae.dambach@gmail.com>
2025-10-21 09:49:15 +0200

    test

---
+++ src/store/reducers.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'react-redux'
+//import other reducer functions and reference below
+
+// assuming this app will grow
+
+const rootReducer = combineReducers({
+    //other reducer functions
+    //test
+})
+
+export default rootReducer
